7c1a51b21e5e0b93efc4ad2438af3a3ba3cd6436
Eirene Fireman <eof@ic.ufal.br>
2021-04-05 20:27:20 -0300

    iniciando atv 3

---
+++ Atividades/Atividade 3/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Empregado empregado1 = new Empregado("Eirene", "Maceió", 10.0);
+        Empregado empregado2 = new Empregado("Adhemar", "Arapiraca", 13.10);
+
+        System.out.println(empregado1.printInformacoesEmpregado(empregado2));
+    
+    }
+}
